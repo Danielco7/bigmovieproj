857a98374208cd84e0e9834ddb55ee955918cbe3
daniel <danielcohenn9@gmail.com>
2022-05-22 18:27:31 +0300

    test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ library.add(fab, faCheckSquare, faUser,faUserGroup,)
 function App() {
   return (
     <div className="App" >
+      didnt wotkk adkljaslk
            <MainComp></MainComp>
     </div>
   );
